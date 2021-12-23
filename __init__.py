020347e7a7ee2ebfa129dc85565c2429f33c009b
Waffle <ox01024@163.com>
2021-12-23 09:49:47 +0800

    update

---
+++ __init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2021/12/18 10:51 PM
+# @Author  : waffle
+# @Email   : ox01024@163.com
+# @File    : __init__.py.py
---
+++ tongdaOA/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/26 22:04
+# @Author  : ox400
+# @Email   : ox01024@163.com
+# @File    : __init__.py.py
